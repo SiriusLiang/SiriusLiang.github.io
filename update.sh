8bb690a0b01da0a479ad5aae1d566aa5f60e1886
Sirius <zenfsharp@163.com>
2024-12-03 10:25:43 +0800

    yolo

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@ scp -r ./docs 2c2g:~/app/docsify/
 scp dockerfile 2c2g:~/app/docsify/
 scp docker-compose.yml 2c2g:~/app/docsify/
 scp ./update_server.sh 2c2g:~/app/docsify/
+
+# run on server
+ssh 2c2g "cd ~/app/docsify/ && ./update_server.sh"
